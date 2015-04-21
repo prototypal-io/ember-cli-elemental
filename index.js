aa556daf98684d5322145868aff8b3a7bec38d89
James Fickel <jamesfickel@gmail.com>
2015-04-21 16:56:06 -0700

    added simple middleware to handle get/post themes

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* jshint node: true */
 'use strict';
+var fs = require('fs');
+var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-theme-editor'
+  name: 'ember-cli-theme-editor',
+  serverMiddleware: function(config) {
+    var app = config.app;
+    var options = config.options;
+    var project = options.project;
+    var themeFile = path.join(__dirname, 'theme.json');
+
+    app.get('theme', function(req, res, next) {
+      fs.exists(themeFile, function(exists) {
+        var output;
+        if (exists) {
+          fs.readFile(themeFile, function(err, data) {
+            res.json(data);
+          });
+        } else {
+          res.json({});
+        }
+      });
+
+      next();
+    });
+
+    app.post('theme', function(req, res, next) {
+      var requestData = '{ "some": "json" }';
+      fs.writeFile(themeFile, requestData, function(err) {
+        if (err) {
+          res.json({ error: err });
+        } else {
+          res.status(200);
+        }
+      });
+      
+      next();
+    });
+  }
 };
