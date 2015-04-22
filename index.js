10c0426598bb618791331d77230eb823dc319af0
James Fickel <jamesfickel@gmail.com>
2015-04-21 23:43:53 -0700

    got demo working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 module.exports = {
   name: 'ember-cli-theme-editor',
@@ -11,32 +12,33 @@ module.exports = {
     var project = options.project;
     var themeFile = path.join(__dirname, 'theme.json');
 
-    app.get('theme', function(req, res, next) {
+    app.use(bodyParser.json());
+
+    app.get('/theme', function(req, res, next) {
       fs.exists(themeFile, function(exists) {
         var output;
         if (exists) {
-          fs.readFile(themeFile, function(err, data) {
+          fs.readFile(themeFile, 'utf-8', function(err, data) {
             res.json(data);
+            next();
           });
         } else {
           res.json({});
+          next();
         }
       });
-
-      next();
     });
 
-    app.post('theme', function(req, res, next) {
-      var requestData = '{ "some": "json" }';
+    app.post('/theme', function(req, res, next) {
+      var requestData = JSON.stringify(req.body, null, '  ');
       fs.writeFile(themeFile, requestData, function(err) {
         if (err) {
           res.json({ error: err });
         } else {
           res.status(200);
         }
+        next();
       });
-      
-      next();
     });
   }
 };
